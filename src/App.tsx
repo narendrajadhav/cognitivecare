c29f1c79324f0d2066c5178c163b0a67aeb5090d
Narendra Jadhav <narendrajadhav105@gmail.com>
2024-07-17 19:04:28 +0530

    Code deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from "react";
+import { AiChat, useAsStreamAdapter } from "@nlux/react";
+import { ToastContainer, toast } from "react-toastify";
+import "@nlux/themes/nova.css";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { send } from "./send";
+import { user, assistantAvatar } from "./personas";
+import { ReactComponent as Microphone } from './microphone.svg';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const adapter = useAsStreamAdapter(send, []);
+  const {
+    transcript,
+    listening,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+  const [startListening, setStartListening] = React.useState(false);
+  const onMircoPhoneClick = () => {
+    window.nluxSimulator.disableSimulator();
+    setStartListening(prevState => {
+      SpeechRecognition[!prevState ? 'startListening' : 'stopListening']();
+
+      return !prevState;
+    });
+  };
+
+  useEffect(() => {
+    if (!listening && transcript) {
+      window.nluxSimulator.setTranscriptValue(transcript);
+      setStartListening(false);
+    }
+  }, [listening]);
+
+
+  useEffect(() => {
+    if (!browserSupportsSpeechRecognition) {
+      toast.error("Error Notification!", {
+        position: 'top-right',
+      });
+    }
+  }, [browserSupportsSpeechRecognition]);
+
+  return (<>
+    <AiChat
+      adapter={adapter}
+      personaOptions={{
+        assistant: {
+          name: "EinAssistant",
+          tagline: "Your Genius AI Assistant",
+          avatar: assistantAvatar,
+        },
+        user,
+      }}
+      conversationOptions={{ layout: "bubbles" }}
+      displayOptions={{ colorScheme: "light", height: '100vh' }}
+      composerOptions={{
+        placeholder: "How may I help you ?",
+      }}
+    />
+    <button className="btn-container">
+      <div className="icon-container">
+        <Microphone
+          onClick={onMircoPhoneClick}
+          className={startListening ? 'microphone-on' : 'microphone-off'}
+        />
+      </div>
+    </button>
+    <ToastContainer />
+  </>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./simulator";
 import reportWebVitals from './reportWebVitals';
+import './index.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import App from "./App";
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
